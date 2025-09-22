3ae83a68ce99f58184bbc6f5f80595e94f0f2c29
Võ Chí Hải <vochihai7@gmail.com>
2025-09-22 20:53:21 +0700

    Update method

--- For/Main.java
+++ For/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         Retangle retangle = new Retangle();
-        System.out.println("Chu vi:" + retangle.cv());
-        System.out.println("Diện tích:" + retangle.dt());
-        System.out.println("Đường chéo:" + retangle.dc());
-        System.out.println("Hình chữ nhật");
+        System.out.println("Perimeter:" + retangle.perimeter());
+        System.out.println("Acreage:" + retangle.acreage());
+        System.out.println("Diagonal:" + retangle.diagonal());
+        retangle.isSquare();
         retangle.draw();
     }
 }
--- For/Retangle.java
+++ For/Retangle.java
@@ -1,53 +1,61 @@
 public class Retangle {
-    private int length;
-    private int width;
+   private int length;
+   private int width;
 
-    public Retangle(int length, int width) {
-        setLength(length);
-        setWidth(width);
-    }
-    public Retangle() {
-        this.length = 10;
-        this.width = 20;
-    }
-    public int getLength() {
-        return length;
-    }
-    public void setLength(int length) {
-        if (length <= 0 || length > 20) {
-            throw  new IllegalArgumentException("Length must be between 1 and 20");
-        }
-        this.length = length;
-    }
-    public int getWidth() {
-        return width;
-    }
-    public void setWidth(int width) {
-        if (width <= 0 || width > 20) {
-            throw  new IllegalArgumentException("Width must be between 1 and 20");
-        }
-        this.width = width;
-    }
+   public Retangle(int length, int width) {
+       setLength(length);
+       setWidth(width);
+   }
 
-    public int cv(){
-        return 2* (length + width) ;
-    }
-    public int dt(){
-        return length * width;
-    }
-    public double dc (){
-        return Math.sqrt(length * length + width * width);
-    }
-    public boolean hv(){
-         return length == width;
-    }
-    public void draw(){
+   public Retangle() {
+       this.length = 15;
+       this.width = 25;
+   }
+
+   public void setLength(int length) {
+       if (length <= 0 || length > 20) {
+        System.out.println("Length must be between 1 and 20");
+       }
+       this.length = length;
+   }
+
+   public void setWidth(int width) {
+       if (width <= 0 || width > 20) {
+           System.out.println("Width must be between 1 and 20");
+       }
+       this.width = width;
+   }
+
+   public int getLength() {
+       return length;
+   }
+
+   public int getWidth() {
+       return width;
+   }
+
+   public int perimeter(){
+       return (length + width) *2;
+   }
+
+   public int acreage(){
+       return length * width;
+   }
+
+   public double diagonal(){
+       return Math.sqrt(length * length + width * width);
+   }
+
+   public boolean isSquare(){
+        return  (length == width);
+   }
+
+   public void draw(){
         for(int i = 0; i < length; i++){
             for(int j = 0; j < width; j++){
-                System.out.print("*");
+                System.out.print('*');
             }
             System.out.print("\n");
         }
-
-    }
+   }
 }
