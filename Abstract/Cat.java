b2c2210144911232b7dc34a0e1ffc60d863ac97d
haivoDA22TTD <vochihai7@gmail.com>
2025-10-01 19:58:11 +0700

    Upload Abstract

---
+++ Abstract/Cat.java
@@ -0,0 +1,3 @@
+abstract class Cat {
+    abstract void speak();
+}
---
+++ Abstract/Cat_While.java
@@ -0,0 +1,5 @@
+public class Cat_While extends Cat {
+    public void speak() {
+        System.out.println("Meo meo");
+    }
+}
