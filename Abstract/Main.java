5f5406cbdb92be71dea47a8ecb8ea5a87397619d
Võ Chí Hải <vochihai7@gmail.com>
2025-10-02 09:35:21 +0700

    Update

--- Abstract/Main.java
+++ Abstract/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       Cat_While cat = new Cat_While();
-       cat.speak();
+       Cat cat = new Cat_While();
+        cat.speak();  
     }
 }
