bc23fc3f8b4013b2f1a5b0f28082f9181cadcc1d
haivoDA22TTD <vochihai7@gmail.com>
2025-10-20 15:26:20 +0700

    Upload Multiplication table

---
+++ multiplication table/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bảng cửu chương");
+        for(int i = 1; i<= 9; i++ ){
+            for(int j = 1; j <= 10; j++ ){
+
+                System.out.printf(" %d * %d = %d\t", i, j, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
